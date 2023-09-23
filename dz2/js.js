ee336229d08c3ff044cb0b1325db0de72ec2dd3a
Zama21 <zama.21@uni-dubna.ru>
2023-09-23 23:09:16 +0300

    rgt

--- dz2/js.js
+++ dz2/js.js
@@ -1,5 +1,6 @@
 let a = Math.floor(Math.random());
 
+a = 13;
 console.log(
   (a > 10 ? a : a * 2) > 5
     ? 2 * a + 1
@@ -9,3 +10,22 @@ console.log(
     ? 6
     : 7
 );
+
+if (a > 10) {
+  if (a > 5) {
+    console.log(2 * a + 1);
+  } else {
+    if (a < 3) {
+      if (1 > 4) {
+        console.log(5);
+      } else if (2 * (a - 2) > 4) {
+        if (a % 2 == 0) {
+          console.log(6);
+        } else {
+          console.log(7);
+        }
+      }
+    }
+  }
+} else {
+}
