a8f0482b53b9bd1a97ee85782168a90558449303
Zama21 <zama.21@uni-dubna.ru>
2023-09-23 22:46:46 +0300

    save

--- dz2/js.js
+++ dz2/js.js
@@ -1,1 +1,11 @@
-console.log('first-push');
+let a = Math.floor(Math.random());
+
+console.log(
+  (a > 10 ? a : a * 2) > 5
+    ? 2 * a + 1
+    : (a < 3 ? 1 : 2 * (a - 2)) > 4
+    ? 5
+    : a % 2 == 0
+    ? 6
+    : 7
+);
