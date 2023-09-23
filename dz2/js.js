87e07daa5fbd328d3c3b78d55490b370351eb2ef
Zama21 <zama.21@uni-dubna.ru>
2023-09-23 23:34:36 +0300

    gfhjfj

--- dz2/js.js
+++ dz2/js.js
@@ -1,6 +1,6 @@
-let a = Math.floor(Math.random());
+let a = Math.floor(Math.random() * 100);
 
-a = 13;
+a = 11;
 console.log(
   (a > 10 ? a : a * 2) > 5
     ? 2 * a + 1
@@ -10,7 +10,7 @@ console.log(
     ? 6
     : 7
 );
-
+console.log('===============================');
 if (a > 10) {
   if (a > 5) {
     console.log(2 * a + 1);
@@ -18,7 +18,17 @@ if (a > 10) {
     if (a < 3) {
       if (1 > 4) {
         console.log(5);
-      } else if (2 * (a - 2) > 4) {
+      } else {
+        if (a % 2 == 0) {
+          console.log(6);
+        } else {
+          console.log(7);
+        }
+      }
+    } else {
+      if (2 * (a - 2) > 4) {
+        console.log(5);
+      } else {
         if (a % 2 == 0) {
           console.log(6);
         } else {
@@ -28,4 +38,29 @@ if (a > 10) {
     }
   }
 } else {
+  if (a * 2 > 5) {
+    console.log(2 * a + 1);
+  } else {
+    if (a < 3) {
+      if (1 > 4) {
+        console.log(5);
+      } else {
+        if (a % 2 == 0) {
+          console.log(6);
+        } else {
+          console.log(7);
+        }
+      }
+    } else {
+      if (2 * (a - 2) > 4) {
+        console.log(5);
+      } else {
+        if (a % 2 == 0) {
+          console.log(6);
+        } else {
+          console.log(7);
+        }
+      }
+    }
+  }
 }
